9f6fc66cb62af09e3a79dd554304311d68677c6a
joseph <younghyun7248@gmail.com>
2024-10-11 22:44:24 +0900

    10/11 수업내용

--- 10_11/quick_sort.py
+++ 10_11/quick_sort.py
@@ -1,6 +1,54 @@
 """
 퀵 솔트
+
+[4, 3, 2, 7, 6, 5]
+
+1차 정렬)
+    list = [4, 3, 2, 7, 6, 5]
+    pivot = 4
+    low_list = [2, 3]
+    high_list = [5, 6, 7]
+
+    return [2, 3, 4, 5, 6, 7]
+
+2차 정렬)
+    list = [3, 2]
+    pivot = 3
+    low_list = [2]
+    high_list = []
+
+    return quick_sort(low) + [pivot] + quick_sort(high)
+    return [2, 3]
+
+
+3차 정렬)
+    list = [2]
+    return [2]
+
+4차 정렬)
+    list = []
+    return []
+
+5차 정렬)
+    list = [7, 6, 5]
+    pivot = 7
+    low_list = [5, 6]
+    high_list = []
+
+    return [5, 6, 7]
+
+6차 정렬)
+    quick_sort(low_list)
+    list = [6, 5]
+
+    pivot = 6
+    low_list = [5]
+    high_list = []
+
+    return [5, 6]
+
 """
+
 
 def quick_sort(unsorted_list: list) -> list:
     if len(unsorted_list) <= 1:
